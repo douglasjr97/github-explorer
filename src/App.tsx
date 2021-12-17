54e0166ed245671ddb2a8bd33a827822131c01f4
Douglas Falcao <falcao.douglas97@gmail.com>
2021-12-17 12:03:13 -0300

    feat: layout base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AppRoutes from './pages/routes';
+import GlobalStyle from './styles/global'
 
 
 const App: React.FC = () => {
   return (
+    <>
     <AppRoutes/>
+    <GlobalStyle/>
+    </>
   )
 }
 
